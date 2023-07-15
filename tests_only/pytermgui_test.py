1c486e46c261dccad08838fe3ff4732274e3dd56
tariqaq <trqphoenix@gmail.com>
2023-07-16 05:19:11 +0530

    tstinggg

--- tests_only/pytermgui_test.py
+++ tests_only/pytermgui_test.py
@@ -1,25 +1,59 @@
-import pytermgui as ptg
+import py_cui
 
-def main():
-    window = ptg.Window()
+class SimpleTodoList:
 
-    input_label = ptg.Label("Enter your input:")
-    input_field = ptg.InputField()
+    def __init__(self, master):
 
-    button_ok = ptg.Button("OK")
-    button_cancel = ptg.Button("Cancel")
+        self.master = master
 
-    window.add_widget(input_label)
-    window.add_widget(input_field)
-    window.add_widget(button_ok)
-    window.add_widget(button_cancel)
+        # The scrolled list cells that will contain our tasks in each of the three categories
+        self.todo_scroll_cell =         self.master.add_scroll_menu('TODO',         0, 0, row_span=6, column_span=2)
+        self.in_progress_scroll_cell =  self.master.add_scroll_menu('In Progress',  0, 2, row_span=7, column_span=2)
+        self.done_scroll_cell =         self.master.add_scroll_menu('Done',         0, 4, row_span=7, column_span=2)
 
-    def on_ok_clicked():
-        print(input_field.get_value())
+        # Textbox for entering new items
+        self.new_todo_textbox = self.master.add_text_box('TODO Item', 6, 0, column_span=2)
 
-    button_ok.on_click(on_ok_clicked)
+        # Keybindings for controlling our CUI.
+        # We bind the enter key for each of the widgets
+        self.new_todo_textbox.add_key_command(          py_cui.keys.KEY_ENTER, self.add_item)
+        self.todo_scroll_cell.add_key_command(          py_cui.keys.KEY_ENTER, self.mark_as_in_progress)
+        self.in_progress_scroll_cell.add_key_command(   py_cui.keys.KEY_ENTER, self.mark_as_done)
+        self.done_scroll_cell.add_key_command(          py_cui.keys.KEY_ENTER, self.remove_item)
 
-    window.run()
+    def add_item(self):
+        """ Add a todo item """
 
-if __name__ == "__main__":
-    main()
+        self.todo_scroll_cell.add_item(f'{self.new_todo_textbox.get()}')
+        self.new_todo_textbox.clear()
+
+    def mark_as_in_progress(self):
+        """ Mark a todo item as inprogress. Remove it from todo scroll list, add it to in progress list, or show error popup if no tasks """
+
+        in_prog = self.todo_scroll_cell.get()
+        if in_prog is None:
+            self.master.show_error_popup('No Item', 'There is no item in the list to mark as in progress')
+            return
+        self.todo_scroll_cell.remove_selected_item()
+        self.in_progress_scroll_cell.add_item(in_prog)
+
+    def mark_as_done(self):
+        """ Mark a inprogress item as done. Remove it from inprogress scroll list, add it to done list, or show error popup if no tasks """
+
+        done = self.in_progress_scroll_cell.get()
+        if done is None:
+            self.master.show_error_popup('No Item', 'There is no item in the list to mark as done')
+            return
+        self.in_progress_scroll_cell.remove_selected_item()
+        self.done_scroll_cell.add_item(done)
+
+    def remove_item(self):
+        """ Remove a todo item """
+
+        self.done_scroll_cell.remove_selected_item()
+
+# Create the CUI with 7 rows 6 columns, pass it to the wrapper object, and start it
+root = py_cui.PyCUI(7, 6)
+root.set_title('CUI TODO List')
+s = SimpleTodoList(root)
+root.start()
