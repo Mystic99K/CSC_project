a2101b07406b4ed1061e8781e249570d2123aa2d
Mystic99K <shashankchodankar5749@gmail.com>
2023-07-17 14:59:18 +0300

    V14 removed

--- v14_alpha/timings.py
+++
@@ -1,8 +0,0 @@
-import time
-import rich.live
-
-def countdown(line, secs):
-    with rich.live.Live() as live:
-        for i in range(secs, 0, -1):
-            live.update(f"{line} {i}")
-            time.sleep(1)
