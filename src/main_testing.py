6d73caead65c6fe6855a9ef37e8f8fd8b7642c02
Jimson Wilson Samuel <jimsonwilson@gmail.com>
2023-08-06 22:30:56 +0300

    More and more testing...

--- src/main_testing.py
+++ src/main_testing.py
@@ -1,20 +1,24 @@
+from rich.panel import Panel
 from rich.console import Console
 from rich.text import Text
 
-
 console = Console()
-
-menu = Text()
 
 tex_color = "#58ACDB"
 bg_color = "#6DE5F2"
 
+menu = Text()
+menu.append(f"""[Currently logged in as Guest]\n""", style="italic red")
+menu.append("1. Login\n")
+menu.append("2. Show weather\n")
+menu.append("3. Options\n")
+menu.append("4. Exit program\n")
 
-menu.append("\n=========================MAIN MENU=========================\n", style=f"{tex_color} on {bg_color}")
-menu += f"""[Currently logged in as Guest]\n"""
-menu += "1. Login\n"
-menu += "2. Show weather\n"
-menu += "3. Options\n"
-menu += "4. Exit program\n"
-
-console.print(menu)
+console.print( 
+    Panel(
+        menu,
+        title="MAIN MENU", 
+        subtitle="",
+        style=f"{tex_color}"
+    )
+)
