73a2279f8bc22a93141022bfa89f3c4058c05c55
tariqaq <trqphoenix@gmail.com>
2023-08-07 22:59:22 +0300

    changing comments

--- src/main_testing.py
+++ src/main_testing.py
@@ -3,30 +3,18 @@
 # from rich.text import Text
 # from rich.prompt import Prompt
 
-console = Console()
-err_console = Console(stderr=True, style="bold red")
+# console = Console()
+# err_console = Console(stderr=True, style="bold red")
 
-tex_color = "#58ACDB"
-bg_color = "#6DE5F2"
+# tex_color = "#58ACDB"
+# bg_color = "#6DE5F2"
 
-menu = Text()
-menu.append(f"""[Currently logged in as Guest]\n""", style="italic red")
-menu.append("1. Login\n")
-menu.append("2. Show weather\n")
-menu.append("3. Options\n")
-menu.append("4. Exit program\n")
-
-console.print( 
-    Panel(
-        menu,
-        title="MAIN MENU", 
-        subtitle="",
-        style=f"{tex_color}"
-    ),
-)
-choice = Prompt.ask("Enter your option")
-
-print(choice, type(choice))
+# menu = Text()
+# menu.append(f"""[Currently logged in as Guest]\n""", style="italic red")
+# menu.append("1. Login\n")
+# menu.append("2. Show weather\n")
+# menu.append("3. Options\n")
+# menu.append("4. Exit program\n")
 
 # console.print( 
 #     Panel(
@@ -40,13 +28,25 @@ print(choice, type(choice))
 
 # print(choice, type(choice))
 
-# # err_console.print( 
-# #     Panel(
-# #         menu,
-# #         title="MAIN MENU", 
-# #         subtitle=""
-# #     )
-# # )
+# console.print( 
+#     Panel(
+#         menu,
+#         title="MAIN MENU", 
+#         subtitle="",
+#         style=f"{tex_color}"
+#     ),
+# )
+# choice = Prompt.ask("Enter your option")
+
+# print(choice, type(choice))
+
+# err_console.print( 
+#     Panel(
+#         menu,
+#         title="MAIN MENU", 
+#         subtitle=""
+#     )
+# )
 
 import rich
 
