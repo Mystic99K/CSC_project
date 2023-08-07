a3e5c8093e18c3a33c53241917cfbcf24ae1a6c2
Mystic99K <shashankchodankar5749@gmail.com>
2023-08-07 22:55:09 +0300

    testing update

--- src/main_testing.py
+++ src/main_testing.py
@@ -3,25 +3,26 @@ from rich.console import Console
 from rich.text import Text
 from rich.prompt import Prompt
 
-console = Console()
+console = Console(color_system='truecolor')
 err_console = Console(stderr=True, style="bold red")
 
-tex_color = "#58ACDB"
-bg_color = "#6DE5F2"
+prompt_color = "#FFF175"
+tex_color = "#64EB51"
+bg_color = "#3E9EFF "
 
 menu = Text()
-menu.append(f"""[Currently logged in as Guest]\n""", style="italic red")
-menu.append("1. Login\n")
-menu.append("2. Show weather\n")
-menu.append("3. Options\n")
-menu.append("4. Exit program\n")
+menu.append(f"""[Currently logged in as Guest]\n""", style=f"{prompt_color}")
+menu.append("1. Login\n", style=f"{tex_color}")
+menu.append("2. Show weather\n", style=f"{tex_color}")
+menu.append("3. Options\n", style=f"{tex_color}")
+menu.append("4. Exit program\n", style=f"{tex_color}")
 
 console.print( 
     Panel(
         menu,
         title="MAIN MENU", 
         subtitle="",
-        style=f"{tex_color}"
+        style=f"{bg_color}"
     ),
 )
 choice = Prompt.ask("Enter your option")
