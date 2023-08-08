55d9b779d9a99a801d3bc98e31e7fb55841e0269
SkyBot <jimsonwilson@proton.me>
2023-08-08 22:44:19 +0300

    print_menu in ui.py.

--- src/main_testing.py
+++ src/main_testing.py
@@ -1,35 +1,14 @@
 from rich.panel import Panel
 from rich.console import Console
-import time
 from rich.text import Text
 from rich.prompt import Prompt
 
 console = Console()
-print("TEsting adwdmawd")
-time.sleep(1)
-console.clear()
-
-
 err_console = Console(stderr=True, style="bold red")
 
-tex_color = "#58ACDB"
-bg_color = "#6DE5F2"
+# tex_color = "#58ACDB"
+# bg_color = "#6DE5F2"
 
-menu = Text()
-menu.append(f"""[Currently logged in as Guest]\n""", style="italic red")
-menu.append("1. Login\n")
-menu.append("2. Show weather\n")
-menu.append("3. Options\n")
-menu.append("4. Exit program\n")
-
-console.print( 
-    Panel(
-        menu,
-        title="MAIN MENU", 
-        subtitle="",
-        style=f"{tex_color}"
-    ),
-)
 choice = Prompt.ask("Enter your option")
 
 # print(choice, type(choice))
