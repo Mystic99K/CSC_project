4d5bcdff4f7ef096006a4f3bb06de51032fd940c
tariqaq <trqphoenix@gmail.com>
2023-08-10 22:53:56 +0300

    more testing in paneltest

--- src/paneltest.py
+++ src/paneltest.py
@@ -6,6 +6,8 @@ from pprint import pprint
 from utils import *
 from rich.console import Console
 
+cls()
+
 console = Console()
 API_KEY = "5c443b217be241e6b75175940230507"
 city = input("Enter city test : ")
@@ -56,9 +58,9 @@ visib_data = f"Visibility(km): {weather_data['current']['vis_km']}\n"
 visib_data += f"Visibility(Miles): {weather_data['current']['vis_miles']}"
 
 # Create three panels
-panel1 = Panel(temperature_data,title='Temperature')
-panel2 = Panel(wind_data,title='Wind')
-panel3 = Panel(visib_data,title='Visiblity')
+panel1 = Panel(temperature_data,title='Temperature', expand=True)
+panel2 = Panel(wind_data,title='Wind', expand=True)
+panel3 = Panel(visib_data,title='Visiblity', expand=True)
 
 # Put them in a Columns object
 columns = Columns([panel1, panel2, panel3], align="center", expand=True)
